de72ce28de90be2c184886d2f14c80d97f8382cf
Parvu Eduard <parvueduard@gmail.com>
2018-12-30 20:55:53 +0200

    fix vertex shader source

--- LearnOpenGlCpp/Main.cpp
+++ LearnOpenGlCpp/Main.cpp
@@ -4,7 +4,7 @@
 
 // global shaders
 const char * vertex_shader_source = "#version 330 core\n"
-"layout (location=0) in vec3 aPos\n"
+"layout (location=0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
 "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
